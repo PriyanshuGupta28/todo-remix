8e3edd0aec1cac5a53a7f7eb63adc54b72da27cb
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-22 17:37:51 +0530

    +remix setup

---
+++ app/routes/_index.tsx
@@ -0,0 +1,19 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Very cool app | Remix" },
+    {
+      property: "og:title",
+      content: "Very cool app",
+    },
+    {
+      name: "description",
+      content: "This app is the best",
+    },
+  ];
+};
+
+export default function Index() {
+  return <>Index</>;
+}
