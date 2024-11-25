51ce1f3e8662da2983f4b697a82a6eeb6c5381fd
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-11-25 17:54:42 +0530

    Create register.service.ts

---
+++ app/services/register.service.ts
@@ -0,0 +1,27 @@
+// import { User } from "~/models/user";
+
+// // Define registerUser function to create a new user
+// export async function registerUser(email: string, password: string) {
+//   try {
+//     // Check if the user already exists by email
+//     const userExists = await User.findOne({ email: email });
+//     if (userExists) {
+//       throw new Error("User already exists");
+//     }
+
+//     // Create a new user
+//     const newUser = new User({
+//       email: email,
+//       password: password,
+//     });
+
+//     await newUser.save();
+//     return newUser;
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       throw new Error("Error during registration: " + error.message);
+//     } else {
+//       throw new Error("An unknown error occurred during registration");
+//     }
+//   }
+// }
